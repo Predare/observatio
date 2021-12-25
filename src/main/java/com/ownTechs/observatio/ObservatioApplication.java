e9c41215718fbbd1866255a8140e7835ce5bf7a2
Predare <predare12000@gmail.com>
2021-12-25 13:07:15 +0300

    First project uploading

---
+++ src/main/java/com/ownTechs/observatio/ObservatioApplication.java
@@ -0,0 +1,13 @@
+package com.ownTechs.observatio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservatioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservatioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ownTechs/observatio/ObservatioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ownTechs.observatio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObservatioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
